358f451136d533aafca4ebf6bfb3bb1c35a23ac0
esilvac <esilva1995@gmail.com>
2018-04-26 12:46:52 -0500

    Create Mapas controller

---
+++ app/controllers/admin/mapas_controller.rb
@@ -0,0 +1,2 @@
+class Admin::MapasController < ApplicationController
+end
---
+++ test/controllers/admin/mapas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::MapasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
