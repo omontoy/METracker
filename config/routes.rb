ea0f1d5a1671bf302534caf109137122e188d5ed
esilvac <esilva1995@gmail.com>
2018-04-26 11:20:51 -0500

    Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+  	resources :mapas, only: [:index] do
+  		get 'completo', on: :collection
+  		get 'especifico', on: :collection
+  	end
+  end
 end
